95228bfc42c2f6b523b279aa08aa5e244997df03
Patainea Robert <patainearobert@yahoo.com>
2021-09-03 16:07:22 +0300

    Alternate chars and design

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,8 @@ export default function App({}: Props): ReactElement {
   const fillCell = (cellIndex: number) => {
     const newCellsContent = [...cellsContent];
     newCellsContent[cellIndex] = nextChar;
-    // if X is was the previous char, set the next one to O
-    // otherwise, set the next to X
     if(nextChar === "X") setNextChar("O")
     else setNextChar("X");
-
     setCellsContent(newCellsContent);
   };
 
@@ -29,22 +26,27 @@ export default function App({}: Props): ReactElement {
   };
 
   return (
-    <div className="grid">
-      <div className="grid-3">
-        {renderGridItem(0)}
-        {renderGridItem(1)}
-        {renderGridItem(2)}
-      </div>
-      <div className="grid-3">
-        {renderGridItem(3)}
-        {renderGridItem(4)}
-        {renderGridItem(5)}
-      </div>
-      <div className="grid-3">
-        {renderGridItem(6)}
-        {renderGridItem(7)}
-        {renderGridItem(8)}
-      </div>
+    <div className = "background">
+        <div className="grid">
+          <div className = "text">
+            Player <span className = "character">{nextChar}</span>'s turn!
+          </div>
+          <div className="grid-3">
+            {renderGridItem(0)}
+            {renderGridItem(1)}
+            {renderGridItem(2)}
+          </div>
+          <div className="grid-3">
+            {renderGridItem(3)}
+            {renderGridItem(4)}
+            {renderGridItem(5)}
+          </div>
+          <div className="grid-3">
+            {renderGridItem(6)}
+            {renderGridItem(7)}
+            {renderGridItem(8)}
+          </div>
+        </div>
     </div>
   );
 }
