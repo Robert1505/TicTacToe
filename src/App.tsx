d6137a71089f704484d5246f5240c03ef74bb1bc
Patainea Robert <patainearobert@yahoo.com>
2021-09-03 16:16:15 +0300

    Fixed clicking cells multiple times

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,12 @@ export default function App({}: Props): ReactElement {
   const [nextChar, setNextChar] = useState("X");
 
   const fillCell = (cellIndex: number) => {
+    if (cellsContent[cellIndex] !== "") 
+      return;
+
     const newCellsContent = [...cellsContent];
     newCellsContent[cellIndex] = nextChar;
-    if(nextChar === "X") setNextChar("O")
+    if (nextChar === "X") setNextChar("O");
     else setNextChar("X");
     setCellsContent(newCellsContent);
   };
@@ -26,27 +29,27 @@ export default function App({}: Props): ReactElement {
   };
 
   return (
-    <div className = "background">
-        <div className="grid">
-          <div className = "text">
-            Player <span className = "character">{nextChar}</span>'s turn!
-          </div>
-          <div className="grid-3">
-            {renderGridItem(0)}
-            {renderGridItem(1)}
-            {renderGridItem(2)}
-          </div>
-          <div className="grid-3">
-            {renderGridItem(3)}
-            {renderGridItem(4)}
-            {renderGridItem(5)}
-          </div>
-          <div className="grid-3">
-            {renderGridItem(6)}
-            {renderGridItem(7)}
-            {renderGridItem(8)}
-          </div>
+    <div className="background">
+      <div className="grid">
+        <div className="text">
+          Player <span className="character">{nextChar}</span>'s turn!
         </div>
+        <div className="grid-3">
+          {renderGridItem(0)}
+          {renderGridItem(1)}
+          {renderGridItem(2)}
+        </div>
+        <div className="grid-3">
+          {renderGridItem(3)}
+          {renderGridItem(4)}
+          {renderGridItem(5)}
+        </div>
+        <div className="grid-3">
+          {renderGridItem(6)}
+          {renderGridItem(7)}
+          {renderGridItem(8)}
+        </div>
+      </div>
     </div>
   );
 }
